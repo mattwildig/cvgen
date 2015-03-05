57ac394d6257c2606ea98621d4e597c7fa3c7c53
Matt Wildig <matt@mattwildig.co.uk>
2015-03-05 16:28:32 +0000

    Add file watch feature
     * Change command line API, usage is
    
           cvgen [opts] infile outfile
    
       rather than writing to stdout.
    
     * Add "watch" feature. If enabled with -w or --watch command line
       option cvgen will wait and watch for changes in the source file,
       automatically recreating output file when they occur.

--- cvgen.gemspec
+++ cvgen.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'cvgen'
-  s.version = '0.0.2'
+  s.version = '0.0.3'
   s.summary = "Generate CVs in PDF format from Markdown-like source"
   s.description = "Because everybody hates word processors"
   s.author = 'Matt Wildig'
@@ -12,4 +12,5 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'prawn', '~> 1.3'
   s.add_runtime_dependency 'prawn-table', '~> 0.2' # Kramdown PDF converter requires this
   s.add_runtime_dependency 'erubis', '~> 2.7'
+  s.add_runtime_dependency 'listen', '~> 2.8'
 end
--- lib/cvgen/exec.rb
+++ lib/cvgen/exec.rb
@@ -7,9 +7,28 @@ module CVGen
 
     def self.call
       config = parse_command_line_args
+      watch = config.delete('watch')
 
-      input = ARGF.read
+      input_file, output_file = ARGV[0], ARGV[1]
 
+      create_output(input_file, output_file, config)
+
+      if watch
+        trap(:INT) { exit }
+
+        require 'listen'
+        Listen.to('.', :only => Regexp.new(input_file)) do |modified, added, removed|
+          #assume the only possibility is modification of source file
+          create_output(input_file, output_file, config)
+        end.start
+
+        puts 'Listening for changes, Ctrl-C to exit'
+        sleep
+      end
+    end
+
+    def self.create_output(input_file, output_file, config)
+      input = File.read(input_file)
       merge_options(config, YAML.load_file('config.yml')) if File.exist? 'config.yml'
 
       if input.start_with? "---\n"
@@ -21,10 +40,8 @@ module CVGen
         merge_options(config, front)
       end
 
-      out = CVGen.generate input, config
-
-      $stdout.write out
-
+      pdf_data = CVGen.generate input, config
+      IO.binwrite(output_file, pdf_data)
     end
 
     def self.merge_options(into, from)
@@ -41,6 +58,10 @@ module CVGen
       OptionParser.new do |opts|
         opts.on("-e", "--enable FEATURES", Array, "List of features to enable") do |features|
           config[FEATURES_OPTION] = features
+        end
+
+        opts.on("-w", "--watch", "Watch for changes and automatically regenerate pdf") do
+          config['watch'] = true
         end
       end.parse!
 
