7f06681d8c7e82b43f6a37011f4dd3f9dc5d44e0
Matt Wildig <matt@mattwildig.co.uk>
2015-03-09 23:07:13 +0000

    Add "sections" support
    Add create_section and section methods to Erb context to allow capturing
    sections of text that can be included in different positions in the
    document. create_section takes a name and a block, the block simply
    being the markup you want to capture. It then stores this to be
    retrieved later with the section method. The section method takes a name
    and returns the previously captured chunk with that name.

--- lib/cvgen.rb
+++ lib/cvgen.rb
@@ -9,7 +9,7 @@ module CVGen
 
   def self.generate source, opts={}
 
-    source = Erubis::Eruby.new(source).evaluate(ErbContext.new(opts.fetch(FEATURES_OPTION, [])))
+    source = Erubis::Eruby.new(source, :bufvar => '@output_buffer').evaluate(ErbContext.new(opts.fetch(FEATURES_OPTION, [])))
 
     root, warnings = Parser.parse(source, opts)
     $stderr.puts warnings if warnings
@@ -22,10 +22,29 @@ module CVGen
 
     def initialize(features)
       @features = features
+      @sections = {}
     end
 
     def method_missing(method, *args)
       @features.include?(method.to_s)
+    end
+
+    def create_section(name)
+      $stderr.puts "Warning: Overwriting section \"#{name}\"" if @sections.has_key?(name)
+
+      buf = ''
+      @output_buffer, temp = buf, @output_buffer
+
+      yield
+
+      @sections[name] = buf
+    ensure
+      @output_buffer = temp
+    end
+
+    def section(name)
+      $stderr.puts "Warning: No section \"#{name}\"" unless @sections.has_key?(name)
+      @sections[name]
     end
 
   end
