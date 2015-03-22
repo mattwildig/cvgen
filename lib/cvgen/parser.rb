2002c7ff44f2a925ff5ca15a97b17bb4047eaa9a
Matt Wildig <matt@mattwildig.co.uk>
2015-03-22 22:35:54 +0000

    Delete commented code
    Comment was code from parent class used as reference.

--- lib/cvgen/parser.rb
+++ lib/cvgen/parser.rb
@@ -6,22 +6,6 @@ module CVGen
     Kramdown::Parser::Kramdown.send :remove_const, :ATX_HEADER_MATCH
     Kramdown::Parser::Kramdown::ATX_HEADER_MATCH = /^(\#{1,6})(.+?(?:\\#)?)\s*?#*#{HEADER_ID}\s*?\n(?:(?:(\S.*)\s*?\n)(?:(\S.*)[ \t]*\n)?)?/
 
-    # def parse_atx_header
-    #   return false if !after_block_boundary?
-
-    #   start_line_number = @src.current_line_number
-    #   @src.check(ATX_HEADER_MATCH)
-    #   level, text, id = @src[1], @src[2].to_s.strip, @src[3]
-    #   return false if text.empty?
-
-    #   @src.pos += @src.matched_size
-    #   el = new_block_el(:header, nil, nil, :level => level.length, :raw_text => text, :location => start_line_number)
-    #   add_text(text, el)
-    #   el.attr['id'] = id if id
-    #   @tree.children << el
-    #   true
-    # end
-
     def parse_atx_header
       rval = super
 
